cddf318d681c73081f2dac7e24553b4c6af051c1
Elisheva Buzaglo <elishevab002@gmail.com>
2026-05-28 11:12:41 +0300

    refactor: load firebase credentials from base64 env variable

--- packages/server/src/config/firebase.js
+++ packages/server/src/config/firebase.js
@@ -1,13 +1,8 @@
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(
-    readFileSync(join(__dirname, '../../firebase-service-account.json'), 'utf8')
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8')
   );
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
